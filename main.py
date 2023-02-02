33159ed5cba973796e5f34866f97b7b34e91ab54
waiga <hyang9387@gmail.com>
2023-02-02 14:40:17 +0900

    csv file update and index, cafe jinja changing

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, SelectField
+from wtforms.fields.html5 import URLField
+from wtforms.validators import DataRequired, URL
 import csv
 from secret_key import SECRET_KEY
 
@@ -12,32 +13,32 @@ Bootstrap(app)
 
 
 class CafeForm(FlaskForm):
-    cafe = StringField('Cafe name', validators=[DataRequired()])
+    cafe = StringField('Cafe Name', validators=[DataRequired()])
+    cafe_location_url = URLField('Cafe URL', validators=[DataRequired(), URL(message='URL 형식에 맞춰서 작성해주세요.')])
+    cafe_open_time = StringField('Opening Time e.g., 8AM', validators=[DataRequired()])
+    cafe_close_time = StringField('Closing Time e.g., 5:30PM', validators=[DataRequired()])
+    coffee_rating = SelectField('Coffee Rating', choices=[('☕️', '☕️'), ('☕️☕️', '☕️☕️'), ('☕️☕️☕️', '☕️☕️☕️'), ('☕️☕️☕️☕️', '☕️☕️☕️☕️'), ('☕️☕️☕️☕️☕️', '☕️☕️☕️☕️☕️')])
+    wifi_rating = SelectField('Wifi Strength Rating', choices=[('🙅', '🙅'), ('💪', '💪️'), ('💪💪', '💪💪'), ('💪💪💪', '💪💪💪'), ('💪💪💪💪', '💪💪💪💪'), ('💪💪💪💪💪', '💪💪💪💪💪')])
+    power_rating = SelectField('Power Socket Availability', choices=[('🙅', '🙅'), ('🔌', '🔌‍'), ('🔌🔌', '🔌‍🔌‍'), ('🔌🔌🔌', '🔌‍🔌‍🔌‍'), ('🔌🔌🔌🔌', '🔌‍🔌‍🔌‍🔌‍'), ('🔌🔌🔌🔌🔌', '🔌‍🔌‍🔌‍🔌‍🔌‍')])
     submit = SubmitField('Submit')
-
-# Exercise:
-# add: Location URL, open time, closing time, coffee rating, wifi rating, power outlet rating fields
-# make coffee/wifi/power a select element with choice of 0 to 5.
-#e.g. You could use emojis ☕️/💪/✘/🔌
-# make all fields required except submit
-# use a validator to check that the URL field has a URL entered.
-# ---------------------------------------------------------------------------
-
 
 # all Flask routes below
 @app.route("/")
 def home():
     return render_template("index.html")
 
-
-@app.route('/add')
+# map link부분 map.naver.com/fkj,0,0,0인 부분이 있어서 csv의 ,와 구분이 안되는 문제가 있음
+@app.route('/add', methods=["GET", "POST"])
 def add_cafe():
     form = CafeForm()
     if form.validate_on_submit():
-        print("True")
-    # Exercise:
-    # Make the form write a new row into cafe-data.csv
-    # with   if form.validate_on_submit()
+        message = f'\n{form.cafe.data}, {form.cafe_location_url.data}, ' \
+                  f'{form.cafe_open_time.data}, {form.cafe_close_time.data}, {form.coffee_rating.data},' \
+                  f'{form.wifi_rating.data}, {form.power_rating.data}'
+        print(message)
+        with open('cafe-data.csv', 'a') as file:
+            file.write(message)
+        return redirect(url_for('cafes'))
     return render_template('add.html', form=form)
 
 
